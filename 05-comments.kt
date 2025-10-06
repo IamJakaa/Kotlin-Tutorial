6f1fb5a08ba3667e4aeb773df53dd87d3f96ec29
Your Name <youremail@example.com>
2025-10-06 17:20:00 +0300

    Add 05-comments.kt example

---
+++ 05-comments.kt
@@ -0,0 +1,29 @@
+fun main(){
+
+
+    // This is a single-line comment
+val name = "Kotlin"  // You can also write comments at the end of a line
+
+
+
+/*
+ This is a multi-line comment.
+ It can span across multiple lines.
+ Useful for documentation or disabling blocks of code.
+*/
+val age = 25
+
+
+
+/**
+ * This function adds two numbers and returns the result.
+ * @param a First number
+ * @param b Second number
+ * @return Sum of a and b
+ */
+fun add(a: Int, b: Int): Int {
+    return a + b
+}
+
+
+}
