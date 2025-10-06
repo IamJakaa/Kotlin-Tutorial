7ff04290a9957e973cad246aa19dc21351b9cd37
Your Name <youremail@example.com>
2025-10-06 16:38:21 +0300

    Add 02-variables.kt example

---
+++ 02-variables.kt
@@ -0,0 +1,13 @@
+fun main() {
+
+    var myVariable: Int = 5
+    println("Initial value: $myVariable")
+
+    println()
+
+    val name: String = "Kotlin"
+    var age: Int = 10
+    println("Hello, $name! You are $age years old.")
+    age += 1
+    println("Next year, you will be $age years old.")
+}
