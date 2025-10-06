690cb1fb07861a8b34151052c25bddd238c5edb7
Your Name <youremail@example.com>
2025-10-06 16:53:42 +0300

    Add 03-data_types.kt examples

---
+++ 03-data_types.kt
@@ -0,0 +1,32 @@
+fun main() {
+
+
+     // Integer types
+    val byteValue: Byte = 10
+    val shortValue: Short = 2000
+    val intValue: Int = 100000
+    val longValue: Long = 10000000000L
+
+    // Floating point types
+    val floatValue: Float = 3.14F
+    val doubleValue: Double = 3.14159265359
+
+    // Character and String
+    val charValue: Char = 'A'
+    val stringValue: String = "Kotlin"
+
+    // Boolean
+    val isActive: Boolean = true
+
+    // Printing values
+    println("Byte Value: $byteValue")
+    println("Short Value: $shortValue")
+    println("Int Value: $intValue")
+    println("Long Value: $longValue")
+    println("Float Value: $floatValue")
+    println("Double Value: $doubleValue")
+    println("Char Value: $charValue")
+    println("String Value: $stringValue")
+    println("Boolean Value: $isActive")
+    
+}
